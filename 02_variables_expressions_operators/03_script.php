73d22f9045da3f6fbef6cc7379a5bc19566b7b32
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2023-07-03 13:00:57 +0200

    Update indexed arrays

--- 02_variables_expressions_operators/03_script.php
+++ 02_variables_expressions_operators/03_script.php
@@ -1,10 +1,14 @@
 <?php
 
+// Use an indexed array when the number of data pieces to be stored is unknown and when you want to store values in a specific order
 // When creating an array without specific keys for each element, PHP assigns them index numbers automatically
 // To store an indexed array in a variable, use a descriptive variable name, the assignment operator, and square brackets
 // Inside the square brackets, list the values the array should hold, separating each value with a comma
 // In the example array below, 'chocolate' is assigned index number zero, and the index increments by one for each subsequent element
 $best_sellers = ['Chocolate', 'Mints', 'Fudge', 'Bubble Gum', 'Toffee', 'Jelly Beans',];
+
+// Update a value in an indexed array by using the variable name holding the array, square brackets with the index number, an assignment operator, and the new desired value
+$best_sellers[0] = 'Cotton Candy'
 ?>
 
 <!DOCTYPE html>
