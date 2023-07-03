b4f37a10c982cc29143aa0e2503f96c97b042d37
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2023-07-03 18:40:10 +0200

    match expressions

---
+++ 03_control_structures/06_script.php
@@ -0,0 +1,27 @@
+<?php
+$day = 'Friday';
+
+// Use the PHP 8 match expression to assign a value to 'offer' based on 'day'
+// If no match is found, the default pattern assigns '10% Off Your Entire Order' to 'offer'
+$offer = match ($day) {
+    'Monday' => '20% Off Chocolates',
+    'Tuesday' => '20% Off Mints',
+    default => '10% Off Your Entire Order',
+}
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Match Expressions</title>
+</head>
+
+<body>
+    <h1>The Sweet Shop</h1>
+    <h2>Offers On <u><?= $day ?></u></h2>
+    <p><?= $offer ?></p>
+</body>
+
+</html>
