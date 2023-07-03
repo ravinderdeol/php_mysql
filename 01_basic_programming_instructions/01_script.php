2b7785b78fbc775e52fa63c9f780dfd6e35e4301
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2023-07-03 11:41:21 +0200

    echo command

---
+++ 01_basic_programming_instructions/01_script.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Echo Command</title>
+</head>
+
+<body>
+    <h1>The Sweet Shop</h1>
+
+    <!-- The 'echo' command in PHP outputs text or markup to the HTML page generated for the browser -->
+    <h2><?php echo 'Ravinder\'s Page'; ?></h2>
+    <p><?php echo 'Offer: 20% Off'; ?></p>
+</body>
+
+</html>
