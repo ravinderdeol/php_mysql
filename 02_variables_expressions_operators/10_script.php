eeb917260d7543d9e1f76aa633a1ffac9281e6e5
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2023-07-03 14:52:13 +0200

    basic php page processing + displaying data

---
+++ 02_variables_expressions_operators/10_script.php
@@ -0,0 +1,42 @@
+<?php
+
+// Variable to hold the username
+$username = 'Ivy';
+
+// Concatenate the username with a greeting
+$greeting = "Hello, $username!";
+
+// Associative array to hold details of the offer
+$offer = [
+    'item' => 'Chocolate',
+    'qty' => 5,
+    'price' => 5,
+    'discount' => 4,
+];
+
+// Calculate the usual price, offer price, saving price, and saving percentage
+$usual_price = $offer['qty'] * $offer['price'];
+$offer_price = $offer['qty'] * $offer['discount'];
+$saving_price = $usual_price - $offer_price;
+$saving_percentage = ($saving_price / $usual_price) * 100;
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>The Sweet Shop</title>
+</head>
+
+<body>
+    <h1>The Sweet Shop</h1>
+    <h2>Multi-Buy Offer</h2>
+
+    <!-- Output the greeting and offer details to the browser -->
+    <p><?= $greeting ?></p>
+    <p>You can save $<?= $saving_price ?> (<?= $saving_percentage ?>%) on your order today.</p>
+    <p>Buy <?= $offer['qty'] ?> packs of <?= $offer['item'] ?> for $<?= $offer_price ?> (usually $<?= $usual_price ?>).</p>
+</body>
+
+</html>
