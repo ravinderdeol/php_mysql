e1951d9e8b583f3344882a47be359db97bfffac0
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2023-07-03 13:36:34 +0200

    echo shorthand

---
+++ 02_variables_expressions_operators/05_script.php
@@ -0,0 +1,22 @@
+<?php
+$name = 'Ivy';
+$favorites = ['Chocolate', 'Toffee', 'Fudge',];
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Echo Shorthand</title>
+</head>
+
+<body>
+    <h1>The Sweet Shop</h1>
+
+    <!-- For outputting a value to the browser using PHP, shorthand for echo can be used when the PHP block is solely intended for that purpose -->
+    <h2>Welcome, <?= $name ?>!</h2>
+    <p>Your favorite sweet is, <?php echo $favorites[0]; ?>.</p>
+</body>
+
+</html>
